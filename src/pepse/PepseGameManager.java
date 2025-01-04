27441122da71bb8465f82925fdd2369199f6deea
Tomer <tomer.zilberman.44@gmail.com>
2025-01-04 19:03:11 +0200

    almost done with Terrain. see TODO to see what is missing

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,9 +1,37 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Block;
+import pepse.world.Sky;
+import pepse.world.Terrain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PepseGameManager extends GameManager {
     public static void main(String[] args) {
         new PepseGameManager().run();
     }
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        gameObjects().addGameObject(sky, Layer.BACKGROUND);
+        Terrain terrain = new Terrain(windowController.getWindowDimensions(), 1);
+        List<Block> blocks = terrain.createInRange(0, (int)windowController.getWindowDimensions().x());
+        for (Block b : blocks) {
+            gameObjects().addGameObject(b, Layer.STATIC_OBJECTS);
+        }
+    }
+
+
+
+
 }
--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -1,4 +1,42 @@
 package pepse.world;
 
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Terrain {
+    private int groundHeightAtX0;
+    private static final int TERRAIN_DEPTH = 20;
+    private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
+
+    public Terrain(Vector2 windowDimensions, int seed) {
+        this.groundHeightAtX0 = Math.round(windowDimensions.y() * 2/3);
+    }
+    //TODO: change it so we can switch the behavior
+    public float groundHeightAt(float x) {
+        return groundHeightAtX0;
+    }
+
+    public List<Block> createInRange(int minX, int maxX) {
+        Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
+        ArrayList<Block> result = new ArrayList<>();
+        for (int i = minX; i < maxX; i+=Block.SIZE) {
+            int highestY = (int)Math.floor(groundHeightAt(60) / Block.SIZE) * Block.SIZE;
+            for (int j = 0; j < TERRAIN_DEPTH; j++) {
+                Block block = new Block(new Vector2(i, highestY + (Block.SIZE * j)), renderable);
+                block.setTag("ground");
+                result.add(block);
+            }
+        }
+        return result;
+    }
+
+
+
 }
