f277e0a0c348f0b14f4b58126c36e75d3b6c24a6
Tomer <tomer.zilberman.44@gmail.com>
2025-01-09 16:48:03 +0200

    Fruit is now class

---
+++ src/pepse/world/trees/Fruit.java
@@ -0,0 +1,12 @@
+package pepse.world.trees;
+
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.world.Block;
+
+public class Fruit extends Block {
+    public Fruit(Vector2 topLeftCorner, Renderable renderable) {
+        super(topLeftCorner, renderable);
+    }
+
+}
